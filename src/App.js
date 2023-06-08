0d10048cd71b2922b5b2ef80a278618bd12cc3ad
tommypurcell <120073087+tommypurcell@users.noreply.github.com>
2023-06-08 13:23:26 +0700

    edamam api works
    get request to edamam api will show calories for one cup of brown rice

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import logo from './logo.svg'
 import './App.css'
-import House from './pages/House'
-import Login from './pages/Login'
-import Profile from './pages/Profile'
-import Houses from './pages/Houses'
-import SignUp from './pages/SignUp'
-import HouseCreate from './pages/HouseCreate'
-import HouseEdit from './pages/HouseEdit'
+import Home from './pages/Home'
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import React from 'react'
-import HouseThumbnail from './components/HouseThumbnail'
+
 function App() {
   return (
     // Router
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Houses />} />
-        <Route path="/house/:id" element={<House />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/house-create" element={<HouseCreate />} />
-        <Route path="/house-edit" element={<HouseEdit />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
