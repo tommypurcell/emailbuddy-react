a3f8a454c7be6144077da0d8fd1c1601646df768
tommypurcell <120073087+tommypurcell@users.noreply.github.com>
2023-06-08 14:57:48 +0700

    Revert "edamam api works"
    This reverts commit 0d10048cd71b2922b5b2ef80a278618bd12cc3ad.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
+import logo from './logo.svg'
 import './App.css'
-import Home from './pages/Home'
+import House from './pages/House'
+import Login from './pages/Login'
+import Profile from './pages/Profile'
+import Houses from './pages/Houses'
+import SignUp from './pages/SignUp'
+import HouseCreate from './pages/HouseCreate'
+import HouseEdit from './pages/HouseEdit'
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import React from 'react'
-
+import HouseThumbnail from './components/HouseThumbnail'
 function App() {
   return (
     // Router
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Houses />} />
+        <Route path="/house/:id" element={<House />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/house-create" element={<HouseCreate />} />
+        <Route path="/house-edit" element={<HouseEdit />} />
       </Routes>
     </BrowserRouter>
   )
