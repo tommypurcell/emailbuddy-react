8014cdb2626c674a33bfdf49fab52ce5602c9272
tommypurcell <120073087+tommypurcell@users.noreply.github.com>
2023-08-05 12:12:35 -0700

    alerts and messages for user
    Let user know that title already exists or that emails have been sent. Confirmation alert to ensure they want to send the emails.

--- src/pages/EmailForm.jsx
+++ src/pages/EmailForm.jsx
@@ -1,9 +1,7 @@
 import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import { Editor } from '@tinymce/tinymce-react'
-
 import Papa from 'papaparse' // Import papaparse
-
 import Nav from '../components/Nav'
 
 axios.defaults.withCredentials = true
@@ -14,12 +12,66 @@ let local_url = 'http://localhost:4000'
 let tiny_api_key = 'y766a0ts9oo4abd6z0ragycw1vj8z485wybl766h9e3xnpek'
 
 export default function EmailForm() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
-  const [csvData, setCsvData] = useState(null)
+  //
+  // state variables
+
   const [message, setMessage] = useState('')
   const [subject, setSubject] = useState('')
   const [subjects, setSubjects] = useState([])
   const [messages, setMessages] = useState([])
+  const [csvData, setCsvData] = useState(null)
+  const [emailData, setEmailData] = useState({})
+  const [emailTitle, setEmailTitle] = useState('')
+  const [titleExists, setTitleExists] = useState('')
+  const [emailsSent, setEmailsSent] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  // send data to server
+  const postEmailData = async (e) => {
+    e.preventDefault()
+
+    // alert and confirm that emails will be sent
+    let confirmation = window.confirm(
+      `Are you sure you want to send ${emailData.recipients.length} emails?`
+    )
+    if (!confirmation) return
+    console.log('sending email data...')
+
+    try {
+      // Send the data to the server using axios
+      const response = await axios.post(
+        `${local_url}/campaigns`,
+        {
+          title: emailData.title,
+          subjects: emailData.subjects, // An array containing the personalized subjects for each recipient
+          messages: emailData.messages, // An array containing the personalized messages for each recipient
+          recipients: emailData.recipients,
+        },
+        {
+          withCredentials: true,
+        }
+      )
+
+      // Assuming the server responds with a success message
+      console.log('Emails sent successfully!', response.data)
+      setEmailsSent(true)
+    } catch (error) {
+      console.error('Error sending emails:', error.message)
+
+      if (
+        error.response &&
+        error.response.data === 'campaign title already exists'
+      ) {
+        // Notify the user that the title is already taken
+        setTitleExists(
+          'The campaign title is already taken. Please choose another title.'
+        )
+      } else {
+        // Handle other types of errors or display a generic message
+        alert('An error occurred while sending the emails.')
+      }
+    }
+  }
 
   // check if user is logged in
   const checkLogin = async () => {
@@ -42,16 +94,32 @@ export default function EmailForm() {
     }
   }
 
+  // set title
+  const populateTitle = (e) => {
+    e.preventDefault()
+    setEmailTitle(e.target.value)
+  }
   // set message based on csv data
-
   const populateMessage = (content) => {
     setMessage(content)
   }
 
   // set subject based on csv data
   const populateSubject = (e) => {
-    e.preventDefault()
-    setSubject(e.target.value)
+    const value = e.target.value
+    setSubject(value)
+  }
+
+  const updateEmailData = () => {
+    setEmailData({
+      title: emailTitle,
+      subjects: subjects, // An array containing the personalized subjects for each recipient
+      messages: messages, // An array containing the personalized messages for each recipient
+      recipients: csvData.slice(1).map((row) => ({
+        name: row[1],
+        email: row[0],
+      })),
+    })
   }
 
   const handleCsvFile = (e) => {
@@ -88,7 +156,7 @@ export default function EmailForm() {
         return message.replace('[Name]', row[1]) // Adjust according to the CSV structure
       })
       setMessages(newMessages.filter(Boolean))
-      console.log('messages', messages)
+      updateEmailData()
     }
   }, [message])
 
@@ -98,11 +166,25 @@ export default function EmailForm() {
         if (index === 0) return null // Skip header row
         return subject.replace('[Name]', row[1]) // Adjust according to the CSV structure
       })
-      setSubjects(newSubjects.filter(Boolean))
-      console.log('subjects', subjects)
-    }
-  }, [subject])
+      console.log('new subjects', newSubjects.filter(Boolean))
 
+      setSubjects(newSubjects.filter(Boolean))
+    }
+  }, [subject, emailTitle])
+
+  useEffect(() => {
+    setTitleExists('')
+  }, [emailTitle])
+
+  useEffect(() => {
+    if (csvData !== null) {
+      updateEmailData()
+      console.log(emailData)
+    }
+  }, [subjects, emailTitle, messages])
+
+  // #TODO
+  // we should check if the current campaign title already exists in the database
   return (
     <>
       <main className="calorie-counter-container m-5">
@@ -116,6 +198,19 @@ export default function EmailForm() {
         ) : (
           <>
             <form>
+              <div className="form-group my-5">
+                <label for="exampleFormControlTextarea1">
+                  Enter a title for this email campaign
+                </label>
+                <textarea
+                  className="form-control"
+                  rows="1"
+                  onKeyUp={populateTitle}
+                ></textarea>
+                {titleExists ? (
+                  <h4 className="text-danger">{titleExists}</h4>
+                ) : null}
+              </div>
               <div className="form-group mb-5">
                 <label for="exampleFormControlTextarea1">
                   Enter email subject line here
@@ -123,6 +218,7 @@ export default function EmailForm() {
                 <textarea
                   className="form-control"
                   rows="1"
+                  value={subject}
                   onChange={populateSubject}
                 ></textarea>
               </div>
@@ -143,8 +239,8 @@ export default function EmailForm() {
                     ],
                     toolbar:
                       'fontsize | undo redo | formatselect | bold italic backcolor | \
-          alignleft aligncenter alignright alignjustify | \
-          bullist numlist outdent indent | removeformat | help',
+                      alignleft aligncenter alignright alignjustify | \
+                      bullist numlist outdent indent | removeformat | help',
                   }}
                 />
               </div>
@@ -183,6 +279,12 @@ export default function EmailForm() {
                 </div>
               </div>
             ))}
+            <button onClick={postEmailData}>
+              Send Emails <i className="fas fa-paper-plane"></i>
+            </button>
+            {emailsSent ? ( // if emails have been sent, show the following
+              <h1>Emails have been sent successfully!</h1>
+            ) : null}
           </>
         )}
       </main>
